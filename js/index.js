d43d544143f28571e7d91711c6b84a89638e9bb0
rahadityaputra <rahadit.a.p123@gmail.com>
2025-01-29 09:08:24 +0700

    memperbaiki link href ke halaman project

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ const createProject = (e) => {
       height: height,
     });
 
-    window.location.href = `/project?id=${project.id}`;
+    window.location.href = `/project/index.html?id=${project.id}`;
   } catch (error) {
     console.log(error);
   }
